2bcb91e9274b84da2123949fe670e874bbcbb721
Ulises Cappa <ulisescappa@gmail.com>
2023-08-16 17:50:38 -0300

    Add propTypes and defaultProps. Fix linters errors

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Calculator from './Components/Calculator';
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,46 +1,53 @@
-/* eslint-disable */
-const Calculator = () => {
-  return (
-    <>
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Calculator = () => (
+
+  <>
     <div className="container">
-      <input className="input-calc" defaultValue={0}></input>
+      <input className="input-calc" defaultValue={0} />
       <div className="buttons">
-        <CalcButton value={'AC'}></CalcButton>
-        <CalcButton value={'+/-'}></CalcButton>
-        <CalcButton value={'%'}></CalcButton>
-        <CalcButton value={'/'}></CalcButton>
 
-        <CalcButton value={7}></CalcButton>
-        <CalcButton value={8}></CalcButton>
-        <CalcButton value={9}></CalcButton>
-        <CalcButton value={'x'}></CalcButton>
+        <CalcButton value="AC" />
+        <CalcButton value="+/-" />
+        <CalcButton value="%" />
+        <CalcButton className="operation" value="/" />
 
-        <CalcButton value={4}></CalcButton>
-        <CalcButton value={5}></CalcButton>
-        <CalcButton value={6}></CalcButton>
-        <CalcButton value={'-'}></CalcButton>
+        <CalcButton className="number" value={7} />
+        <CalcButton className="number" value={8} />
+        <CalcButton className="number" value={9} />
+        <CalcButton className="operation" value="x" />
 
-        <CalcButton value={1}></CalcButton>
-        <CalcButton value={2}></CalcButton>
-        <CalcButton value={3}></CalcButton>
-        <CalcButton value={'+'}></CalcButton>
-        
-        <CalcButton className={'cero'} value={'0'}></CalcButton>
-        <CalcButton value={'.'}></CalcButton>
-        <CalcButton className="equal" value={'='}></CalcButton>
+        <CalcButton className="number" value={4} />
+        <CalcButton className="number" value={5} />
+        <CalcButton className="number" value={6} />
+        <CalcButton className="operation" value="-" />
+
+        <CalcButton className="number" value={1} />
+        <CalcButton className="number" value={2} />
+        <CalcButton className="number" value={3} />
+        <CalcButton className="operation" value="+" />
+
+        <CalcButton className="number cero" value="0" />
+        <CalcButton value="." />
+        <CalcButton className="operation equal" value="=" />
       </div>
-      
+
     </div>
-    
+
   </>
-  )
-  
+);
+
+const CalcButton = ({ value, className }) => <button type="button" className={className}>{value}</button>;
+
+CalcButton.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  className: PropTypes.string,
 };
 
-
-const CalcButton = ({value, className}) => {
-  return <button className={className}>{value}</button>;
-}
-/* eslint-enable */
+CalcButton.defaultProps = {
+  value: '',
+  className: '',
+};
 
 export default Calculator;
