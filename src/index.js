6a971bc0b06051d9e5686e7db9b189ad8b977212
Ulises Cappa <ulisescappa@gmail.com>
2023-08-16 21:39:31 -0300

    Create buttons.js fot the chuld component.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
