3a10d63b1cd365f6056d3be9dfef448aa1b4cc56
Ulises Cappa <ulisescappa@gmail.com>
2023-08-19 17:15:22 -0300

    Add message error if it is something went wrong.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Quotes from './Components/Quotes';
 function App() {
   return (
     <>
-    <Quotes />
-    <Calculator />
+      <Quotes />
+      <Calculator />
     </>
   );
 }
--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -1,39 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function Quotes() {
   const API_KEY = 'PfgoD6eJJrdbX0H0Rpd90w==rNfC856dJmfCqHLG';
-  const [quote, setQuote] = useState({ category: "" });
-  const [isLoading, setIsLoading] = useState(false)
+  const [quote, setQuote] = useState({ category: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setIsLoading(true)
-        const res = await fetch("https://api.api-ninjas.com/v1/quotes?category=learning", {
+        setIsLoading(true);
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=learning', {
           method: 'GET',
           headers: {
-            'X-Api-Key': API_KEY, 
+            'X-Api-Key': API_KEY,
             'Content-Type': 'application/json',
           },
         });
         const json = await res.json();
         setQuote(json[0]);
-        setIsLoading(false)
+        setIsLoading(false);
       } catch (error) {
-        console.error("Error fetching quotes:", error);
-        setIsLoading(false)
+        setHasError(error);
+        setIsLoading(false);
       }
     };
     fetchData();
-  }, []); 
+  }, []);
 
-  if (isLoading){
-    console.log('que pasa aca');
-    return <div>Loading...</div>
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-  
+
   return (
-    <p> {quote.quote}- {quote.author}</p>
+    <div>
+      {hasError ? (
+        <p>Something went wrong! Please verify the connection.</p>
+      ) : (
+        <>
+          <p>{quote.quote}</p>
+          <span>
+            -
+            {quote.author}
+          </span>
+        </>
+      )}
+    </div>
   );
 }
 
