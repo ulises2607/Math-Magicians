06c1520bc8c6a40cf2ee3a1f5c2145e474887674
Ulises Cappa <ulisescappa@gmail.com>
2023-08-19 03:00:59 -0300

    Reorganized and refactored calculator. Calculate.js is used to successfully get the operations. Screen.js is added, which works as its display. Styles are added for each component.

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,9 +1,53 @@
-import React from 'react';
-import DisplayButtons from './buttons';
+import React, { useState } from 'react';
+import '../Styles/Calculator.css';
+import Button from './Button';
+import Screen from './Screen';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [input, setInput] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const addInput = (val) => {
+    setInput(calculate(input, val));
+  };
+
   return (
-    <DisplayButtons />
+    <div className="calculator-container">
+      <Screen input={input} />
+      <div className="row">
+        <Button handleClick={addInput}>AC</Button>
+        <Button handleClick={addInput}>+/-</Button>
+        <Button handleClick={addInput}>%</Button>
+        <Button handleClick={addInput}>÷</Button>
+      </div>
+      <div className="row">
+        <Button handleClick={addInput}>7</Button>
+        <Button handleClick={addInput}>8</Button>
+        <Button handleClick={addInput}>9</Button>
+        <Button handleClick={addInput}>x</Button>
+      </div>
+      <div className="row">
+        <Button handleClick={addInput}>4</Button>
+        <Button handleClick={addInput}>5</Button>
+        <Button handleClick={addInput}>6</Button>
+        <Button handleClick={addInput}>-</Button>
+      </div>
+      <div className="row">
+        <Button handleClick={addInput}>1</Button>
+        <Button handleClick={addInput}>2</Button>
+        <Button handleClick={addInput}>3</Button>
+        <Button handleClick={addInput}>+</Button>
+      </div>
+      <div className="row last">
+        <Button handleClick={addInput} className="cero">0</Button>
+        <Button handleClick={addInput}>.</Button>
+        <Button handleClick={addInput}>=</Button>
+      </div>
+    </div>
   );
 }
 
